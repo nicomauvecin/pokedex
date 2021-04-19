edb18b32e9b664ce11b3dd4b697576fa6a524c1f
Nicolas <medinamauvecin@gmail.com>
2021-04-19 15:43:50 -0300

    Agrega función de mostrar datos del pokemon seleccionado

--- src/index.js
+++ src/index.js
@@ -61,4 +61,50 @@ $botonAnterior.onclick = function(){
         return;      
 };
 
+const $tituloModal = document.querySelector('#titulo-nombre-pokemon');
+const $contenidoModal = document.querySelector('.modal-body');
+
+$contenedorPokemon.onclick = function(e){
+    let pokemonSeleccionado = e.target;
+    if (pokemonSeleccionado.className === 'foto-pokemon' || pokemonSeleccionado.className === 'nombre-pokemon'){
+        pokemonSeleccionado = e.target.parentNode;
+    } 
+    if (pokemonSeleccionado.className === 'row'){
+        return;
+    }
+    pokemonSeleccionado.setAttribute('data-target', '#info-pokemon');
+    pokemonSeleccionado.setAttribute('data-toggle', 'modal');
+
+    const $nombrePokemonSeleccionado = pokemonSeleccionado.querySelector('.nombre-pokemon').innerText;
+    const URL_fotoPokemonSeleccionado = pokemonSeleccionado.querySelector('img').getAttribute('src');
+
+    const $fotoPokemonSeleccionado = $contenidoModal.querySelector('img');
+    const $pesoPokemonSeleccionado = $contenidoModal.querySelector('#peso-pokemon-seleccionado');
+    const $alturaPokemonSeleccionado = $contenidoModal.querySelector('#altura-pokemon-seleccionado');
+    const $tiposPokemonSeleccionado = $contenidoModal.querySelector('#tipo-pokemon-seleccionado');
+    const $habilidadPokemonSeleccionado = $contenidoModal.querySelector('#habilidad1-pokemon-seleccionado');
+    
+    function cargarDatos(){
+        $tituloModal.innerText = `${$nombrePokemonSeleccionado}`;
+        $fotoPokemonSeleccionado.setAttribute('src', `${URL_fotoPokemonSeleccionado}`)
+
+        fetch(`${API_URL}pokemon/${$nombrePokemonSeleccionado}`)
+        .then(respuesta => respuesta.json())
+        .then(respuestaJSON =>{
+            $pesoPokemonSeleccionado.innerText = `Peso: ${(respuestaJSON.weight*0.1).toFixed(2)} kg.`
+            $alturaPokemonSeleccionado.innerText = `Altura: ${(respuestaJSON.height*0.1).toFixed(2)} m.`
+            
+            if (respuestaJSON.types.length = 1){
+                $tiposPokemonSeleccionado.innerText = `Tipo: ${respuestaJSON.types[0].type.name}`
+            } else {
+                $tiposPokemonSeleccionado.innerText = `Tipos: ${respuestaJSON.types[0].type.name} y ${respuestaJSON.types[1].type.name}`
+            }
+
+            $habilidadPokemonSeleccionado.innerText = `Habilidades: ${respuestaJSON.abilities[0].ability.name} y ${respuestaJSON.abilities[1].ability.name}`
+        });
+
+       }
+    cargarDatos();
+}
+
 cargarInicio();
